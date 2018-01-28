5264840e12d156cb4e9f3d5e4fd05c7cacdc21f3
Gregory Sims <briggybros.2@googlemail.com>
2018-01-28 17:02:41 +0000

    Recklessly fixed prop type validation

--- src/ExternalStyles.jsx
+++ src/ExternalStyles.jsx
@@ -1,7 +1,17 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { StyleSheetManager } from 'styled-components';
+import { CONTEXT_KEY } from 'styled-components/lib/models/StyleSheet';
 
 import ExternalBrowserStyleSheet from './ExternalBrowserStyleSheet';
+
+StyleSheetManager.propTypes = {
+    sheet: PropTypes.any.isRequired,
+};
+
+StyleSheetManager.childContextTypes = {
+    [CONTEXT_KEY]: PropTypes.any.isRequired,
+};
 
 const ExternalStyles = ({ children, element }) => {
     const sheet = ExternalBrowserStyleSheet.create(element);
